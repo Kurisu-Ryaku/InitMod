df2067f679650e0fefab9079ba7d928be487f6a1
nnf123 <niannf123@gmail.com>
2023-09-05 18:30:30 +0800

    ADD README FILE

--- src/main/java/name/init_mod/InitMod.java
+++ src/main/java/name/init_mod/InitMod.java
@@ -1,5 +1,6 @@
 package name.init_mod;
 
+import name.init_mod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,7 +10,9 @@ public class InitMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("init-mod");
+
+	public static final String MOD_ID = "init-mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +20,6 @@ public class InitMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Init Mod!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/name/init_mod/item/ModItems.java
@@ -0,0 +1,11 @@
+package name.init_mod.item;
+
+import name.init_mod.InitMod;
+
+//这是一个注册物品的辅助类
+public class ModItems {
+
+    public static void registerModItems(){
+        InitMod.LOGGER.info("Registering Mod Items for" + InitMod.MOD_ID);
+    }
+}
