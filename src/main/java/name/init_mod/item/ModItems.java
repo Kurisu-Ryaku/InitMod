9022398781c2e1a9bc16df9bdef784282ca12983
nnf123 <niannf123@gmail.com>
2023-09-05 18:39:28 +0800

    CREATED ITEM BUILD CLASS

--- src/main/java/name/init_mod/item/ModItems.java
+++ src/main/java/name/init_mod/item/ModItems.java
@@ -1,9 +1,18 @@
 package name.init_mod.item;
 
 import name.init_mod.InitMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 //这是一个注册物品的辅助类
 public class ModItems {
+
+    //向Fabric注册新物品
+    private static Item registerItem (String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(InitMod.MOD_ID, name), item);
+    }
 
     public static void registerModItems(){
         InitMod.LOGGER.info("Registering Mod Items for" + InitMod.MOD_ID);
