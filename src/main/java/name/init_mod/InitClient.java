1d73f94aba78ace6ad9847b41e9c8f5a7d0d6207
nnf123 <niannf123@gmail.com>
2023-09-05 16:46:12 +0800

    Initial commit

---
+++ src/main/java/name/init_mod/InitClient.java
@@ -0,0 +1,10 @@
+package name.init_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InitClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
