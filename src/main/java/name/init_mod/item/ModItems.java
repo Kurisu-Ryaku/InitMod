95b58db93dcd8b0f836c3afc4c5091520171f10d
nnf123 <niannf123@gmail.com>
2023-09-06 09:52:11 +0800

    ADD NEW ITEM THIGH

--- src/main/java/name/init_mod/item/ModItems.java
+++ src/main/java/name/init_mod/item/ModItems.java
@@ -1,7 +1,12 @@
 package name.init_mod.item;
 
 import name.init_mod.InitMod;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -9,7 +14,13 @@ import net.minecraft.util.Identifier;
 //这是一个注册物品的辅助类
 public class ModItems {
 
-    public static final String a = "asd";
+    //在这里声明物品
+    public static final Item THIGH = registerItem("thigh", new Item(new FabricItemSettings()));
+
+    //将物品添加到物品分类组
+    private static void addItemsToItemIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(THIGH);
+    }
 
     //向Fabric注册新物品
     private static Item registerItem (String name, Item item) {
@@ -18,5 +29,7 @@ public class ModItems {
 
     public static void registerModItems(){
         InitMod.LOGGER.info("Registering Mod Items for" + InitMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToItemIngredientItemGroup);
     }
 }
