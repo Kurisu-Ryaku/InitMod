20e2db6491b36549e964090f69b3f22295a4f21d
nnf123 <niannf123@gmail.com>
2023-09-09 17:54:56 +0800

    ADD LOOT TABLE FOR EACH ITEM

--- src/main/java/name/init_mod/block/ModBlocks.java
+++ src/main/java/name/init_mod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import name.init_mod.InitMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.block.Block;
@@ -11,21 +12,30 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
-    //新建一個方塊， 使用copyOf讓它擁有和IRON_BLCOK相同的屬性（挖掘時間、爆炸抗性...）又覆蓋其sound為紫水晶
+    //BLOCKS
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK))
-            );
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK))
-    );
+            new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
 
     //新建一個方塊， 使用create自定義其屬性
 //    public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
 //            new Block(FabricBlockSettings.create().)
 //    );
+
+    //ORES(DROPS EXPERIENCES)
+    public static final Block RUBY_ORE = registerBlock("ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE).strength(2f), UniformIntProvider.create(20, 400)));
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_DIAMOND_ORE).strength(2.3f), UniformIntProvider.create(20, 400)));
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHERRACK).strength(1.2f), UniformIntProvider.create(20, 400)));
+    public static final Block END_RUBY_ORE = registerBlock("end_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.END_STONE).strength(1.4f), UniformIntProvider.create(20, 400)));
 
     //添加方塊
     private static Block registerBlock(String name, Block block) {
